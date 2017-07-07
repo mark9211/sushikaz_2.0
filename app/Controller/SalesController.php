57885f5747a73f2f25589670074584bb538729ab
Mark SATO <mark.sato1111@gmail.com>
2017-07-07 10:47:21 +0900

    からのatemdamce_results非表示

--- app/Controller/SalesController.php
+++ app/Controller/SalesController.php
@@ -1148,17 +1148,22 @@ class SalesController extends AppController{
 				#アソシ二段階
 				$this->AttendanceResult->recursive = 2;
 				$attendance_results = $this->AttendanceResult->find('all', array(
-					'conditions' => array('AttendanceResult.location_id' => $location['Location']['id'], 'working_day' => $working_day)
+					'conditions' => array(
+						'AttendanceResult.location_id' => $location['Location']['id'],
+						'AttendanceResult.working_day' => $working_day,
+					)
 				));
 				#勤務時間帯挿入
 				$new_attendance_results = array();
 				foreach($attendance_results as $attendance_result){
-					$attendance_result['timezone'] = $this->AttendanceResult->judgeLunchDinner($attendance_result);
-					#休憩時間
-					$hours = (strtotime($attendance_result['AttendanceResult']['attendance_end']) - strtotime($attendance_result['AttendanceResult']['attendance_start'])) / (60 * 60);
-					$hours = $hours - $attendance_result['AttendanceResult']['hours'] - $attendance_result['AttendanceResult']['late_hours'];
-					$attendance_result['break'] = $hours;
-					$new_attendance_results[] = $attendance_result;
+					if($attendance_result['Member']!=null){
+						$attendance_result['timezone'] = $this->AttendanceResult->judgeLunchDinner($attendance_result);
+						#休憩時間
+						$hours = (strtotime($attendance_result['AttendanceResult']['attendance_end']) - strtotime($attendance_result['AttendanceResult']['attendance_start'])) / (60 * 60);
+						$hours = $hours - $attendance_result['AttendanceResult']['hours'] - $attendance_result['AttendanceResult']['late_hours'];
+						$attendance_result['break'] = $hours;
+						$new_attendance_results[] = $attendance_result;
+					}
 				}
 				$this->set('attendance_results', $new_attendance_results);
 				//////////////////////////////////////////////////////////////////////////////////////////////////////////
